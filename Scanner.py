43e9e6de60c942a632bbb23a949e0b075969b43d
Uknow-95 <margot@xn--80aabqk5atp.com>
2026-08-29 17:31:49 +0200

    Port Scan Python (Basique)
    Il scan les ports choisis par l'utilisateur sur la plage choisie

---
+++ Scanner.py
@@ -0,0 +1,20 @@
+import socket
+
+class Scanner:
+    def __init__(self, target, port_range):
+        self.target = target
+        self.port_range = port_range
+        self.results = []
+
+    def scan(self):
+        for port in self.port_range:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(1)
+            result = sock.connect_ex((self.target, port))
+            sock.close()
+            if result == 0:
+                self.results.append(port)
+        return self.results
+
+mon_scan = Scanner(input("IP OR DOMAINE: "), range(1, 100))
+print(mon_scan.scan())
